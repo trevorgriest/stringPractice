51c8f4bca5232cba922185e6caa50707b07eea32
@trevorgriest <tgriest1981@gmail.com>
2019-09-04 14:43:06 -0400

    NamesAdded

--- stringPractice2/Program.cs
+++ stringPractice2/Program.cs
@@ -10,6 +10,18 @@ namespace stringPractice2
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("What is your first name?");
+
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("Your firstname is: " + firstname);
+            
+            Console.WriteLine("What is your last name?");
+
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("Your last name is: " + lastname);
         }
     }
 }
